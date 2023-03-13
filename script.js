b20455651484f3508e493d0260df52b22f080561
dmitro-t <tokarievdmitriy@gmail.com>
2023-03-13 17:44:37 +0200

    Использование Local Storage

--- script.js
+++ script.js
@@ -14,6 +14,37 @@ function showSelectedInfo(){
     priceTotal.textContent = checked * ticketPrice;
 }
 
+function setMovieInfoToLS(){
+    let index = selectedMovie.selectedIndex;
+    let price = selectedMovie.value;
+    localStorage.setItem('indexMovie', index);
+    localStorage.setItem('priceMovie', price);
+}
+
+// Запускается функция при перезагрузке страницы
+getFromLS();
+showSelectedInfo();
+
+// получаем из LocStor
+function getFromLS(){
+    let selectedSeats = JSON.parse(localStorage.getItem('indexSelectedSeats'));
+    // console.log(selectedSeats.indexOf(5))
+    // Если что то есть в LocStor
+    if(selectedSeats !== null && selectedSeats.length > 0){
+        seats.forEach(function(elem, index){
+            // в index передается поочередно ИНДЕКСЫ эл-тов seat есть ли в selectedSeats такое ЗНАЧЕНИЕ, если нет возвращает -1 
+            if(selectedSeats.indexOf(index) != -1){
+                elem.classList.add('selected');
+            }
+        })
+    }
+     
+    let selectedMovieIndex = localStorage.getItem('indexMovie');
+    if(selectedMovieIndex !== null){
+        selectedMovie.selectedIndex = selectedMovieIndex;
+    }
+}
+
 // Ставим EventListener на родителя для того чтобы получать информацию на что нажали в пределах этого контейнера
 hallContainer.addEventListener('click', (event) => {
     // Отслеживаем на что нажали - event.target
@@ -22,11 +53,22 @@ hallContainer.addEventListener('click', (event) => {
         event.target.classList.toggle('selected');
         showSelectedInfo();
     }
+
+    // Хранение в LocStor индексы выбранных мест
+    let selectedSeats = document.querySelectorAll('.row .seat.selected');
+    indexSelectedSeats = [...selectedSeats].map(function(elem){
+        return [...seats].indexOf(elem);
+    });
+   
+    localStorage.setItem('indexSelectedSeats', JSON.stringify(indexSelectedSeats));
+
 })
+    
 
 // EventListener на селекторе
 selectedMovie.addEventListener('change', event => {
     ticketPrice = event.target.value;
 
     showSelectedInfo();
+    setMovieInfoToLS();
 })
